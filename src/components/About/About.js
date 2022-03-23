c040567617b30606884edfd0c710a0cc2c4d979b
ThatKidd17 <60009707+ThatKidd17@users.noreply.github.com>
2022-03-23 12:51:19 -0400

    Update About.js
    added info for about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,11 +11,17 @@ const About = () => {
           <div>
             <h1>About</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+            From a single office that opened in 1962 in Toronto, Ontario, Fake Estate LLC has grown into one of the leading global real estate franchisors,</p><p> with over 135,000 agents in more than 115 countries and territories
             </p>
+            <img src={require("../../assets/neighbourhood.png")}/>
+            <p>
+            The global network of franchisee-owned and -operated offices are run with the world's most productive real estate sales agents.
+            </p>
+            <img src={require("../../assets/header.jpeg")}/>
+            <p>
+            Customer service built on a foundation of drive, experience and education  is the cornerstone of Fake Estate success. Backed by the ability to deliver,</p><p> remains as important as ever.
+            </p>
+            <Footer />
           </div>
         </div>
       </div>
