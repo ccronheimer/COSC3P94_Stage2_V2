a46bcd8bc9e6edbaa43be18944559cd59c26c887
ThatKidd17 <60009707+ThatKidd17@users.noreply.github.com>
2022-04-20 13:32:38 -0400

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,12 @@ import Calculator from "./Calculator/Calculator";
 import About from "./About/About";
 import Home from "./Home/Home";
 import Footer from "./Footer/Footer";
+import Isabelle from "./Isabelle/Isabelle";
+import Brice from "./Brice/Brice";
+import James from "./James/James";
+import John from "./John/John";
+import Lana from "./Lana/Lana";
+import Michelle from "./Michelle/Michelle"
 function App() {
   return (
     <div className="App">
@@ -15,10 +21,14 @@ function App() {
         <Route path="/COSC3P94_Stage2/buy" element={<Buy />} />
         <Route path="/COSC3P94_Stage2/sell" element={<Sell />} />
         <Route path="/COSC3P94_Stage2/agents" element={<Agents />} />
-
         <Route path="/COSC3P94_Stage2/calculator" element={<Calculator />} />
-
         <Route path="/COSC3P94_Stage2/about" element={<About />} />
+        <Route path="/COSC3P94_Stage2/isabelle" element={<Isabelle />} />
+        <Route path="/COSC3P94_Stage2/james" element={<James />} />
+        <Route path="/COSC3P94_Stage2/brice" element={<Brice />} />
+        <Route path="/COSC3P94_Stage2/lana" element={<Lana />} />
+        <Route path="/COSC3P94_Stage2/john" element={<John />} />
+        <Route path="/COSC3P94_Stage2/michelle" element={<Michelle />} />
       </Routes>
       <Footer/>
     </div>
